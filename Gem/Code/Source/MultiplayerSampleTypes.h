9a3b696083e82f4894b0adfd1e5f6b177230342c
AMZN-Olex <5432499+AMZN-Olex@users.noreply.github.com>
2022-08-09 09:26:19 -0500

    Added a const for maximum supported players
    Signed-off-by: AMZN-Olex <5432499+AMZN-Olex@users.noreply.github.com>

--- Gem/Code/Source/MultiplayerSampleTypes.h
+++ Gem/Code/Source/MultiplayerSampleTypes.h
@@ -45,6 +45,8 @@ namespace MultiplayerSample
 
     using RoundTimeSec = AzNetworking::QuantizedValues<1, 2, 0, 3600>; // 1 hour max round duration
 
+    static constexpr int MaxSupportedPlayers = 8;
+
     // Temporary match player state.
     struct PlayerCoinState
     {
