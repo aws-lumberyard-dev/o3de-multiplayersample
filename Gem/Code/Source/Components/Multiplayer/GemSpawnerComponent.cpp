1abca5f1a669a52a3a7900118b289df50de62483
AMZN-Olex <5432499+AMZN-Olex@users.noreply.github.com>
2022-08-31 21:08:43 -0500

    Hiding gems until removal is fixed
    Signed-off-by: AMZN-Olex <5432499+AMZN-Olex@users.noreply.github.com>

--- Gem/Code/Source/Components/Multiplayer/GemSpawnerComponent.cpp
+++ Gem/Code/Source/Components/Multiplayer/GemSpawnerComponent.cpp
@@ -181,8 +181,12 @@ namespace MultiplayerSample
 
             if (netEntityHandle.Exists())
             {
-                AZ::EntityBus::MultiHandler::BusConnect(gem.first);
-                Multiplayer::GetNetworkEntityManager()->MarkForRemoval(netEntityHandle);
+                // Currently blocked by https://github.com/o3de/o3de/issues/11695
+                /*AZ::EntityBus::MultiHandler::BusConnect(gem.first);
+                Multiplayer::GetNetworkEntityManager()->MarkForRemoval(netEntityHandle);*/
+
+                // Until https://github.com/o3de/o3de/issues/11695 is fixed, move the gem out of the view
+                AZ::TransformBus::Event(gem.first, &AZ::TransformBus::Events::SetWorldTranslation, AZ::Vector3::CreateAxisZ(-1000.f));
 
                 m_queuedForRemovalGems.emplace(gem.first, gem.second);
             }
